19ae33ec4febc99319e3a67b83a3dcffc2312f45
Bezaev Roman <31104632+trixdade@users.noreply.github.com>
2018-11-28 15:21:00 +0300

    Ferma test, mod operations
    Some troubles with templates :(

--- 01.PrimeFactors/bezaev_roman/Main.cpp
+++ 01.PrimeFactors/bezaev_roman/Main.cpp
@@ -1,41 +1,149 @@
 #include <iostream>
+#include <random>
 #include <cmath>
 #include <vector>
 #include <ctime>
+#include <algorithm>
 #include <fstream>
 #include "BigIntegerLibrary.hh"
 
 using namespace std;
+// Modulo methods
+template <class T>
+T Sum(T &a, T &b, T &mod);
+template <class T>
+T Sub(T &a, T &b, T &mod);
+template <class T>
+T Mul(T &a, T &b, T &mod);
+template <class T>
+T Inverse(T &a, T &mod);
+template <class T> 
+T Pow(T a, T n, T m);
 
-class ModInt {
-private:
-	BigInteger value, mod;
-public:
-	ModInt(BigInteger &_x, BigInteger &_mod) : value(_x), mod(_mod) {}
-	ModInt(const ModInt &modint) : value(modint.value) , mod(modint.mod) {}
-	BigInteger operator+(const BigInteger &b) const;
-	BigInteger operator-(const BigInteger &b) const;
-};
+template <class T>
+T mul_inv(T a, T b);
 
-BigInteger ModInt::operator+(const BigInteger &b) const {
-	return 0;
+// Prime methods
+template <class T>
+void prime_divisors(T numb);
+template <class T>
+bool isPrime_simple(T x);
+
+template <class T>
+bool ferma(T x);
+
+
+int main() {
+	ifstream fin("in_primeFactors\\prime9_in.txt");
+	ofstream fout("out_primeFactors\\prime9_out.txt");
+
+	string s = "12312312312";
+	BigInteger a = stringToBigInteger(s);
+	cout << ferma(a) << endl;
+	system("pause");
 }
 
-BigInteger ModInt::operator-(const BigInteger &b) const {
-	return 0;
+template <class T> // сложнение по модулю
+T Sum(T &a, T &b, T &mod) {
+	return (a + b) % mod;
 }
 
-int isPrime(BigInteger x) {
-	if (x == 2) return 1;
-	for (BigInteger i = 2; i*i < x; i++) {
-		if (x % i == 0)
-			return 0;
+template <class T> // вычитание по модулю
+T Sub(T &a, T &b, T &mod) {
+	if (a >= b) {
+		return (a - b) % mod;
 	}
-	return 1;
+	else {
+		return a - b + mod;
+	}
 }
 
-void prime_divisors(BigInteger numb) {
-	BigInteger divisor = 2;
+template <class T> // умножение по модулю
+T Mul(T &a, T &b, T &mod) {
+	return (a * b) % mod;
+}
+
+template <class T> // быстрое возведение в степень по модулю
+T Pow(T a, int n, T m) {
+	T r = 1;
+	while (n) {
+		if (n % 2 == 1)
+			r = Mul(r, a, m);
+		a = Mul(a, a, m);
+		n /= 2;
+	}
+	return r;
+}
+
+template <class T> // поиск обратного по модулю
+T Inverse(T &a, T &mod) {
+	if (!(mod%a))
+		throw (string)"This number don't have inverse";
+	a = a % mod;
+	for (int x = 1; x < mod; x++)
+		if ((a * x) % mod == 1)
+			return x;
+}
+
+
+
+template <class T> // пробный поиск обратного из интернета
+T mul_inv(T a, T b)
+{
+	T b0 = b, t, q;
+	T x0 = 0, x1 = 1;
+	if (b == 1) return 1;
+	while (a > 1) {
+		q = a / b;
+		t = b, b = a % b, a = t;
+		t = x0, x0 = x1 - q * x0, x1 = t;
+	}
+	if (x1 < 0) x1 += b0;
+	return x1;
+}
+
+const int k = 100;
+template <class T>
+bool ferma(T x) {
+	if (x == 2)
+		return true;
+	srand(time(NULL));
+	for (int i = 0; i < k; i++) {
+		T rand(rand());
+		T a = (rand % (x - 2)) + 2;
+		if (gcd(a, x) != 1)
+			return false;
+		if (Pow(a, x - 1, x) != 1)
+			return false;
+	}
+	return true;
+}
+
+template <class T>
+T gcd(T a, T b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a%b);
+}
+
+template <class T> // простая проверка числа на простоту
+bool isPrime_simple(T x) {
+	if (x == 2) return 1;
+	T i = 2;
+	while (i*i <= x) {
+		if (x % i == 0)
+			return false;
+		i += 2;
+		if (i % 100000 == 0) {
+			cout << i << endl;
+		}
+	}
+	return true;
+}
+
+template <class T> // простое разложение числа на множители
+void prime_divisors(T numb) {
+	T divisor = 2;
 	while (divisor*divisor <= numb)
 	{
 		if (numb%divisor == 0)
@@ -48,24 +156,4 @@ void prime_divisors(BigInteger numb) {
 		else divisor = divisor + 2;
 	}
 	cout << numb;
-}
-
-
-int main() {
-	std::string s;
-	//ifstream fin("in_primeFactors\\random20_in.txt");
-	//ofstream fout("out_primeFactors\\random20_out.txt");
-	BigInteger x;
-	int start = clock();
-	s = "11967867867890780";
-	x = stringToBigInteger(s);
-	prime_divisors(x);
-	int end = clock();
-	cout << endl;
-	int time = (end - start);
-	cout << time << endl;
-
-	
-
-	system("pause");
 }
